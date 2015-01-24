8f49a3071545b826fe35fc29f28be3ce0e13e24e
Nithanth Kudige <nithanth@gmail.com>
2015-01-23 21:02:15 -0500

    Fade-outs

--- public/javascripts/client.socket.io.js
+++ public/javascripts/client.socket.io.js
@@ -8,24 +8,28 @@ $(document).ready(function() {
 		console.log('Client: Sent ping request to server');
 		document.getElementById("ping-sent-c").innerHTML = parseInt(document.getElementById("ping-sent-c").innerHTML) + 1
 		document.getElementById("pong-res-1").innerHTML = "PING...";
+		$('#pong-res-1').fadeOut(1500);
 	});
 });
 
 server.on('pong-from-server', function(data) {
     console.log('Client: Pong received from server' );
 	document.getElementById("pong-res-1").innerHTML = "PONG!";
+	$('#pong-res-1').fadeOut(1500);
 	document.getElementById("pong-rec-c").innerHTML = parseInt(document.getElementById("pong-rec-c").innerHTML) + 1;
 });
 
 server.on('ping-from-server', function(data) {
 	console.log('Client: Ping received from server');
 	document.getElementById("ping-req-2").innerHTML = "PING...";
+	$('#ping-req-2').fadeOut(1500);
 	document.getElementById("ping-rec-c").innerHTML = parseInt(document.getElementById("ping-rec-c").innerHTML) + 1;
 
 	setTimeout(function() {
 	    server.emit('pong-from-client');
 		console.log('Client: Sent pong response to server');
 		document.getElementById("pong-res-2").innerHTML = "PONG!";
+		$('#pong-res-2').fadeOut(1500);
 		document.getElementById("pong-sent-c").innerHTML = parseInt(document.getElementById("pong-sent-c").innerHTML) + 1;
     }, 200 + Math.round(2000 * Math.random()));
 });
